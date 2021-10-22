60a0fdf7c002bcf8d1ea0950cdf198fe2fde2096
Leandro Henrique <80431106+Leandro00023@users.noreply.github.com>
2021-10-22 10:57:25 -0300

    Versão 1.0
    Primeira aula do projeto (21/10/2021)

---
+++ src/test/java/br/com/leandrosampaio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrosampaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
